3aa31b71b2d0a5f48eaa7a1be1df66beb280ec48
Fabien Townsend <fabientownsend@gmail.com>
2018-05-20 19:46:18 +0100

    Use extracted method for step_1

--- step_1/tests/temperature.test.c
+++ step_1/tests/temperature.test.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "../lib/unity_fixture.h"
 
-#include "../src/myheader.h"
+#include "../src/temperature.h"
 
 TEST_GROUP(Temperature);
 TEST_SETUP(Temperature) { }
 TEST_TEAR_DOWN(Temperature) { }
-
-float ConvertCelciusToFahrenheit(float celcius);
-float ConvertCelciusToKelvin(float celcius);
 
 TEST(Temperature, CelciusToFahreneit)
 {
@@ -34,14 +31,4 @@ static void runAllTests() {
 int main(int argc, const char* argv[])
 {
   UnityMain(argc, argv, runAllTests);
-}
-
-float ConvertCelciusToFahrenheit(float celcius)
-{
-  return celcius * 1.8 + 32;
-}
-
-float ConvertCelciusToKelvin(float celcius)
-{
-  return celcius += 273.15;
 }

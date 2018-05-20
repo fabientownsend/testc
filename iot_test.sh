83d22f5d62d59f3da1ecfc149691418a6e4fa68f
Fabien Townsend <fabientownsend@gmail.com>
2018-05-20 11:48:59 +0100

    Build software for temperature

---
+++ iot_test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gcc unity_fixture.c myheader.c temperature.c  unity.c -o main && ./main
---
+++ temperature.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "unity_fixture.h"
+
+#include "myheader.h"
+
+TEST_GROUP(Temperature);
+TEST_SETUP(Temperature) { }
+TEST_TEAR_DOWN(Temperature) { }
+
+float ConvertCelciusToFahrenheit(float celcius);
+float ConvertCelciusToKelvin(float celcius);
+
+TEST(Temperature, CelciusToFahreneit)
+{
+  float celcius = 1;
+  float kelvin = 33;
+
+  TEST_ASSERT_EQUAL_INT(ConvertCelciusToFahrenheit(celcius), kelvin);
+}
+
+TEST(Temperature, CelciusToKelvin)
+{
+  float celcius = 1;
+  float kelvin = 274;
+
+  TEST_ASSERT_EQUAL_INT(ConvertCelciusToKelvin(celcius), kelvin);
+}
+
+static void runAllTests() {
+  RUN_TEST_CASE(Temperature, CelciusToKelvin);
+  RUN_TEST_CASE(Temperature, CelciusToFahreneit);
+
+}
+
+int main(int argc, const char* argv[])
+{
+  UnityMain(argc, argv, runAllTests);
+}
+
+float ConvertCelciusToFahrenheit(float celcius)
+{
+  return celcius * 1.8 + 32;
+}
+
+float ConvertCelciusToKelvin(float celcius)
+{
+  return celcius += 273.15;
+}

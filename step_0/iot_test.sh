c44dcedcaf9e1776b9a5bda2030d2d9ed6e02619
Fabien Townsend <fabientownsend@gmail.com>
2018-05-20 19:24:10 +0100

    Makes step 0 is just one test fails

--- step_0/iot_test.sh
+++ step_0/iot_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcc lib/unity_fixture.c src/myheader.c tests/temperature.test.c lib/unity.c -o main && ./main
+gcc lib/unity.c lib/unity_fixture.c tests/example.test.c -o main && ./main
